f5e83f213e49442bab4faddcb5938f58c04437ba
Ray Ploski <ray@redhat.com>
2021-03-12 15:24:19 -0800

    Added pre-commit formatting to terraform

--- pihole/terraform/pihole.tf
+++ pihole/terraform/pihole.tf
@@ -12,33 +12,33 @@ when we reboot our node.
 resource "kubernetes_persistent_volume" "dnsmasq_pv" {
   metadata {
     name = "dnsmasq-pv"
-    labels  = {
-        type = "local"
-        arch = "ARM64"
+    labels = {
+      type = "local"
+      arch = "ARM64"
     }
   }
   spec {
-      capacity = {
-          storage = "4Gi"
+    capacity = {
+      storage = "4Gi"
+    }
+    access_modes       = ["ReadWriteMany"]
+    storage_class_name = "local-storage"
+    persistent_volume_source {
+      local {
+        path = "/data/k8s-pv/dnsmasq"
       }
-      access_modes = ["ReadWriteMany"]
-      storage_class_name = "local-storage"
-      persistent_volume_source {
-          local {
-              path = "/data/k8s-pv/dnsmasq"
+    }
+    node_affinity {
+      required {
+        node_selector_term {
+          match_expressions {
+            key      = "kubernetes.io/hostname"
+            operator = "In"
+            values   = ["enceladus"]
           }
+        }
       }
-      node_affinity {
-          required {
-              node_selector_term {
-                  match_expressions {
-                      key = "kubernetes.io/hostname"
-                      operator = "In"
-                      values = ["enceladus"]
-                  }
-              }
-          }
-      }
+    }
   }
 }
 
@@ -47,22 +47,22 @@ resource "kubernetes_persistent_volume_claim" "dnsmasq_pvc" {
   metadata {
     name = "dnsmasq-pvc"
     labels = {
-      app = "pihole"
+      app       = "pihole"
       component = "dnsmasq-pvc"
-      type = "local"
+      type      = "local"
     }
     namespace = "pihole"
   }
   spec {
-      access_modes = ["ReadWriteMany"]
-      resources {
-          requests = {
-              storage = "4Gi"
-          }
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "4Gi"
       }
+    }
 
-      storage_class_name = "local-storage"
-      volume_name = "dnsmasq-pv"
+    storage_class_name = "local-storage"
+    volume_name        = "dnsmasq-pv"
   }
 }
 
@@ -74,33 +74,33 @@ when we reboot our node.
 resource "kubernetes_persistent_volume" "pihole_pv" {
   metadata {
     name = "pihole-pv"
-    labels  = {
-        type = "local"
-        arch = "ARM64"
+    labels = {
+      type = "local"
+      arch = "ARM64"
     }
   }
   spec {
-      capacity = {
-          storage = "4Gi"
+    capacity = {
+      storage = "4Gi"
+    }
+    access_modes       = ["ReadWriteMany"]
+    storage_class_name = "local-storage"
+    persistent_volume_source {
+      local {
+        path = "/data/k8s-pv/pihole"
       }
-      access_modes = ["ReadWriteMany"]
-      storage_class_name = "local-storage"
-      persistent_volume_source {
-          local {
-              path = "/data/k8s-pv/pihole"
+    }
+    node_affinity {
+      required {
+        node_selector_term {
+          match_expressions {
+            key      = "kubernetes.io/hostname"
+            operator = "In"
+            values   = ["enceladus"]
           }
+        }
       }
-      node_affinity {
-          required {
-              node_selector_term {
-                  match_expressions {
-                      key = "kubernetes.io/hostname"
-                      operator = "In"
-                      values = ["enceladus"]
-                  }
-              }
-          }
-      }
+    }
   }
 }
 
@@ -111,21 +111,21 @@ resource "kubernetes_persistent_volume_claim" "pihole_pvc" {
   metadata {
     name = "pihole-data-pvc"
     labels = {
-      app = "pihole"
+      app  = "pihole"
       type = "local"
     }
     namespace = "pihole"
   }
   spec {
-      access_modes = ["ReadWriteMany"]
-      resources {
-          requests = {
-              storage = "2Gi"
-          }
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "2Gi"
       }
+    }
 
-      storage_class_name = "local-storage"
-      volume_name = "pihole-pv"
+    storage_class_name = "local-storage"
+    volume_name        = "pihole-pv"
   }
 }
 
@@ -133,11 +133,11 @@ resource "kubernetes_persistent_volume_claim" "pihole_pvc" {
 
 
 resource "helm_release" "pihole_helm" {
-    name = "pihole"
-    chart = "../helm/"
-    namespace = "pihole"
-    values = [
-        "${file("pihole-values.yml")}"
-    ]
+  name      = "pihole"
+  chart     = "../helm/"
+  namespace = "pihole"
+  values = [
+    "${file("pihole-values.yml")}"
+  ]
 
 } 
